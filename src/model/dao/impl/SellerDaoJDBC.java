7de4e9b82991cb6e6cb4087a83d479cd4efc6f9c
Alexsander Kafka <alexsanderkafka2001alex@gmail.com>
2023-04-24 10:58:21 -0300

    Add o método deleteById

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -99,6 +99,26 @@ public class SellerDaoJDBC implements SellerDao{
 	@Override
 	public void deleteById(Integer id) {
 		
+		PreparedStatement st = null;
+		
+		try {
+			
+			st = conn.prepareStatement("DELETE FROM seller WHERE Id = ?");
+			
+			st.setInt(1, id);
+			
+			int rowns = st.executeUpdate();
+			
+			if (rowns == 0) {
+				System.out.println("Id no exist");;
+			}
+		}
+		catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+		}
 	}
 
 	@Override
